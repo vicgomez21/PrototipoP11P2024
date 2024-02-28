cc2eddf3dba947ce928d92fef215bd32b74941bd
vico <victorgomezcarrascosa@gmail.com>
2024-02-28 10:11:09 -0600

    segundo commit

--- PrototipoP11P2024/main.cpp
+++ PrototipoP11P2024/main.cpp
@@ -7,22 +7,31 @@
 
  #include <iomanip> //sirve para formatos de salida
 using namespace std;
-#define EQUIPOS 6
-#define COLUMAS 2
-#define MAX_VOTOS 10
-#define PIDA_VUELTA 10
+#define EQUIPOS 5
+#define COLUMAS 3
+#define MAX_VOTOS 50
+
 
 void imprimirMatriz(int matriz[EQUIPOS][COLUMAS+1]);
 
 struct TablaGeneral{
 string nombre;
-int PJ;
-int PG;
-int Pe;
-int Pp;
+int P2010;
+int P2011;
+int p2012;
+int p2014;
 int Puntos;
-};
 
+};
+struct LigaAscenso{
+string nombre;
+int P2010;
+int P2011;
+int P2014;
+int P2012;
+int Puntos;
+
+};
 //La sección abarca una variedad de estructuras de control en programación
 //, comenzando con una introducción sobre su importancia. Se detallan las estructuras if, if...else y switch,
 
@@ -38,24 +47,44 @@ int busquedaAleatorios(int minimo, int maximo) {
 // explicando su uso para tomar decisiones en el programa. Luego, se exploran las estructuras de repetición,
 //incluyendo while, do...while y for. Se analiza el anidamiento de estructuras de control para acciones más complejas, y se introduce la declaración de variables dentro de bucles. También se cubre la terminación anticipada de bucles y se presentan las estructuras de control break, continue.
 void imprimirMatriz(int matriz[EQUIPOS][COLUMAS+1]){
-    Tabla_ida_vuelta t_ida_vuelta[PIDA_VUELTA];
+
     TablaGeneral T_general[EQUIPOS];
+    LigaAscenso liga_asenso[EQUIPOS];
         bool repetir=true;
         char opcion;
 
 do{
     for(int i =0;i<EQUIPOS;i++){
-        cout<<"Equipo No " <<i+1 <<" de la tabla general"<<endl;
+        cout<<"Equipo No " <<i+1 <<" de la tabla de Liga Nacional"<<endl;
         cout<<"Nombre: ";
         cin>>T_general[i].nombre;
 
-        T_general[i].PJ=10;
+        T_general[i].P2010=busquedaAleatorios( 1,MAX_VOTOS);
 
-        T_general[i].PG=busquedaAleatorios( 1,5);
-        T_general[i].Pe=busquedaAleatorios( 1,5);
-        T_general[i].Pp=10-(T_general[i].PG+T_general[i].Pe);
+        T_general[i].P2011=busquedaAleatorios( 1,MAX_VOTOS);
+        T_general[i].p2012=busquedaAleatorios( 1,MAX_VOTOS);
+        T_general[i].p2014=busquedaAleatorios( 1,MAX_VOTOS);
 
-        T_general[i].Puntos=(T_general[i].PG*3)+T_general[i].Pe;
+        T_general[i].Puntos=(T_general[i].P2010+T_general[i].P2011+T_general[i].p2012+T_general[i].p2014);
+
+
+
+}
+
+
+        //-----------------------------------------------------------
+for(int i =0;i<EQUIPOS;i++){
+        cout<<"Equipo No " <<i+1 <<" de la tabla de Liga Asenso"<<endl;
+        cout<<"Nombre: ";
+        cin>>liga_asenso[i].nombre;
+
+        liga_asenso[i].P2010=busquedaAleatorios( 1,MAX_VOTOS);
+
+        liga_asenso[i].P2011=busquedaAleatorios( 1,MAX_VOTOS);
+        liga_asenso[i].P2012=busquedaAleatorios( 1,MAX_VOTOS);
+        liga_asenso[i].P2014=busquedaAleatorios( 1,MAX_VOTOS);
+
+        liga_asenso[i].Puntos=(liga_asenso[i].P2010+liga_asenso[i].P2011+liga_asenso[i].P2012+liga_asenso[i].P2014);
 
 
         //-----------------------------------------------------------
@@ -67,6 +96,11 @@ int Mepuntos=100000000;
 string Equipo_Menor;
 string Equipo_Mayor;
 int suma=0;
+int Mpuntos1=0;
+int Mepuntos1=100000000;
+string Equipo_Menor1;
+string Equipo_Mayor1;
+int suma1=0;
 
 for(int i=0;i< EQUIPOS;i++){
 //acumulador de votos
@@ -86,25 +120,62 @@ if (T_general[i].Puntos<Mepuntos){
 }
 
 }
+//segundo for
+//-------------------------------------------------------------------
+for(int i=0;i< EQUIPOS;i++){
+//acumulador de votos
+suma1=suma1+liga_asenso[i].Puntos;
+
+//mayor votos//
 //----------------------------------------------------------------------------//----------------------------------------------------------------------------
-cout<<setw(10)<<"------Tabla general 2024-----"<<endl<<endl;
-cout<<setw(9)<<"Nombre /"<<setw(9)<<" JJ"<<setw(15)<<"JG "<< setw(9) << "JP" << setw(15) << "JE" << setw(15) << "Puntos" <<  endl<<endl;
+if(liga_asenso[i].Puntos>Mpuntos1){
+Mpuntos1=liga_asenso[i].Puntos;
+Equipo_Mayor1=liga_asenso[i].nombre;
+}
+//menor votos
+//----------------------------------------------------------------------------//----------------------------------------------------------------------------
+if (liga_asenso[i].Puntos<Mepuntos1){
+    Mepuntos1=liga_asenso[i].Puntos;
+    Equipo_Menor1=liga_asenso[i].nombre;
+}
+
+}
+//----------------------------------------------------------------------------//----------------------------------------------------------------------------
+cout<<setw(10)<<"------Historial Liga Nacional-----"<<endl<<endl;
+cout<<setw(9)<<"Nombre /"<<setw(9)<<" Torneo 2010/"<<setw(15)<<"Torneo 2011/"<< setw(9) << "Torneo 2012/" << setw(15) << "Torneo 2014/" << setw(15) << "PTS acumulados/" <<  endl<<endl;
   cout<<"------------------------------------------------------------------------------------------"<<endl;
     //for para imprimir los datos
     for(int i=0;i<EQUIPOS;i++){
-     cout<<setw(9)<<T_general[i].nombre<<setw(9)<<T_general[i].PJ<<setw(15)<<T_general[i].PG<< setw(9) << T_general[i].Pp << setw(15) << T_general[i].Pe << setw(15) <<T_general[i].Puntos <<  endl<<endl;
+     cout<<setw(9)<<T_general[i].nombre<<setw(9)<<T_general[i].P2010<<setw(15)<<T_general[i].P2011<< setw(9) << T_general[i].p2012 << setw(15) << T_general[i].p2014 << setw(15) <<T_general[i].Puntos <<  endl<<endl;
     }
-    cout<<"el equipo GANADOR ES "<<Equipo_Mayor <<" con "<< Mpuntos<<" PTS"<<endl<<endl;
-    cout<<"el EQUIPO que DESCIENDE ES "<<Equipo_Menor <<" con "<<Mepuntos<<" PTS"<<endl<<endl;
+    cout<<"EL EQUIPO "<<Equipo_Mayor <<" TIENE  "<< Mpuntos<<" PTS Y ES LA PUNTUACION MAS ALTA"<<endl<<endl;
+    cout<<"EL EQUIPO "<<Equipo_Menor <<" TIENE "<<Mepuntos<<" PTS Y ES LA PUNTUACION MAS ALTA"<<endl<<endl;
+
+
+//----------------------------------------------------------------------------------------------------------------
+cout<<setw(10)<<"------Historial Liga Ascenso-----"<<endl<<endl;
+cout<<setw(9)<<"Nombre /"<<setw(9)<<" Torneo 2010/"<<setw(15)<<"Torneo 2011/"<< setw(9) << "Torneo 2012/" << setw(15) << "Torneo 2014/" << setw(15) << "PTS acumulados/" <<  endl<<endl;
+  cout<<"------------------------------------------------------------------------------------------"<<endl;
+    //for para imprimir los datos
+    for(int i=0;i<EQUIPOS;i++){
+     cout<<setw(9)<<liga_asenso[i].nombre<<setw(9)<<liga_asenso[i].P2010<<setw(15)<<liga_asenso[i].P2011<< setw(9) << liga_asenso[i].P2012 << setw(15) << liga_asenso[i].P2014 << setw(15) <<liga_asenso[i].Puntos <<  endl<<endl;
+    }
+    cout<<"EL EQUIPO "<<Equipo_Mayor1 <<" TIENE  "<< Mpuntos1<<" PTS Y ES LA PUNTUACION MAS ALTA"<<endl<<endl;
+    cout<<"EL EQUIPO "<<Equipo_Menor1 <<" TIENE "<<Mepuntos1<<" PTS Y ES LA PUNTUACION MAS ALTA"<<endl<<endl;
+
+
+
  cout << "Desea otro calculo (s/n)? ";
         cin >> opcion;
         system("cls");
         if (opcion == 'n') {
             repetir = false;    // Si la opción es 'n', se termina el bucle
         }
-    } while (repetir);
+    }while (repetir);
 
 
 
 }
+
+
 
